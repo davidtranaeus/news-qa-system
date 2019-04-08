cfb5da2fa82852b9231d099746441dbdb9959b7d
davidtranaeus <davidtra@kth.se>
2019-04-08 14:03:48 +0200

    Move word2vec

--- utils/setup_w2v.py
+++ utils/setup_w2v.py
@@ -4,11 +4,11 @@ from gensim.models import KeyedVectors
 if __name__ == "__main__":
 
   model = gensim.models.KeyedVectors.load_word2vec_format(
-    'data/google-word2vec/GoogleNews-vectors-negative300.bin', 
+    '../data/google-word2vec/GoogleNews-vectors-negative300.bin', 
     binary=True,
     limit=500000)
 
-  path = "data/wordvectors.kv"
+  path = "../data/wordvectors.kv"
   model.wv.save(path)
 
-  wv = KeyedVectors.load("data/google-word2vec/wordvectors.kv", mmap='r')
+  # wv = KeyedVectors.load("../data/google-word2vec/wordvectors.kv", mmap='r')
--- w2v_test.py
+++
@@ -1,8 +0,0 @@
-from gensim.models import KeyedVectors
-from sklearn.metrics.pairwise import cosine_similarity
-import sys
-
-if __name__ == "__main__":
-  wv = KeyedVectors.load("data/wordvectors.kv", mmap='r')
-  print(wv.similarity(sys.argv[1], sys.argv[2]))
-  
